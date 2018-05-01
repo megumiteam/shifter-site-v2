5cfc9124b89be86e85f51931fa1ac52d926f891b
Daniel Olson <emaildano@gmail.com>
2018-05-01 14:35:40 -0400

    updated pattern lib

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './assets/scss/App.css';
+import './assets/dc-pattern-lib/scss/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

07878a7f2198e155895bd2c981ac20b7fbc5be6a
Daniel Olson <emaildano@gmail.com>
2018-05-01 14:54:11 -0400

    moved logos files to pattern lib, updated nav

--- src/containers/header/nav-primary.jsx
+++ src/containers/header/nav-primary.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Logo from "./../logos/logos";
+import Logo from "./../../assets/dc-pattern-lib/react/components/logos/logos";
 import {
   Container,
   Collapse,
@@ -28,52 +28,54 @@ class PrimaryNav extends Component {
   }
 
   render() {
-    return <Navbar expand="md" className="_gradient-purple-dark navbar-dark navbar-expand-md text-uppercase">
-        <Container>
-          <NavbarBrand href="/">
-            <Logo LogoStyle="primary-color-white" />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="mr-auto ml-5" navbar>
-              <NavItem>
-                <NavLink href="/features/">Features</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/pricing/">Pricing</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/blog/">Blog</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink target="_blank" href="https://support.getshifter.io">
-                  Support
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink target="_blank">Contact</NavLink>
-              </NavItem>
-            </Nav>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="https://go.getshifter.io">Login</NavLink>
-              </NavItem>
-              <NavItem className="d-none d-lg-block">
-                <NavLink className="px-0 text-lowercase" href="https://go.getshifter.io">
-                  or
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="pr-0" href="#">
-                  <Button className="btn btn-gradient-primary text-uppercase btn-arrow">
-                    Sign Up
-                  </Button>
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>;
+    return <div className="_gradient-purple-dark nav-primary">
+        <Navbar expand="md" className="navbar-dark navbar-expand-md text-uppercase">
+          <Container>
+            <NavbarBrand href="/">
+              <Logo LogoStyle="primary-color-white" />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="mr-auto ml-5" navbar>
+                <NavItem>
+                  <NavLink href="/features/">Features</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/pricing/">Pricing</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/blog/">Blog</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink target="_blank" href="https://support.getshifter.io">
+                    Support
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink target="_blank">Contact</NavLink>
+                </NavItem>
+              </Nav>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="https://go.getshifter.io">Login</NavLink>
+                </NavItem>
+                <NavItem className="d-none d-lg-block">
+                  <NavLink className="px-0 text-lowercase" href="https://go.getshifter.io">
+                    or
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="pr-0" href="#">
+                    <Button className="btn btn-gradient-primary text-uppercase btn-arrow">
+                      Sign Up
+                    </Button>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './assets/dc-pattern-lib/scss/main.css';
 
+document.body.classList.add("shifter--main");
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

fa779df7d3d43a2d699467f11ed2ac56369cf32e
Daniel Olson <emaildano@gmail.com>
2018-06-07 17:25:23 -0400

    updated style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import('../src/assets/dc-pattern-lib/scss/main.css');
+import 'dc-pattern-lib/scss/main.css';
 
 document.body.classList.add("shifter--main");
 

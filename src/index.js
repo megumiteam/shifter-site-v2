aef417b344d37278ee36138b3eb28e602ae31694
Daniel Olson <emaildano@gmail.com>
2018-07-25 19:36:28 -0400

    updated theme settings, fixed blog style, added conditional for local env

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import 'dc-pattern-lib/scss/main.css';
 
-import('../src/assets/dc-pattern-lib/scss/main.css');
+const hostname = window && window.location && window.location.hostname;
+
+if ( hostname === 'localhost' ) {
+  import('../src/assets/main.css');
+} else {
+  import('dc-pattern-lib/scss/main.css');
+}
 
 document.body.classList.add("shifter--main");
 

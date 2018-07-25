b5e384dec1ad4fddb481e123cadc2820a14bcecf
Daniel Olson <emaildano@gmail.com>
2018-07-25 19:39:38 -0400

    set default

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import('dc-pattern-lib/scss/main.css');
 
 const hostname = window && window.location && window.location.hostname;
 
 if ( hostname === 'localhost' ) {
   import('../src/assets/main.css');
-} else {
-  import('dc-pattern-lib/scss/main.css');
 }
 
 document.body.classList.add("shifter--main");
